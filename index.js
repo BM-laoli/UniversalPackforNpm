278b6ec631ddb8ac63486dea3400af16b76bc4d9
bmlaoli <861795660@qq.com>
2020-05-28 10:24:42 +0800

    跨域&静态资源&Post数据解决方案‘

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+    // 跨域解决方案
+app.use(cors())
 
+// POST解决方案
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// 文件上传的文件夹模块配置（或者说，静态文件的解决方案）
+app.use('/uploads', express.static(__dirname + '/uploads')) //静态路由
 
 
 
